971946b81086aff77bcc2e01d037ddc72773110c
Lakshay Angrish <lakshay.angrish@hotmail.com>
2020-09-04 18:59:07 +0530

    add basic get() for hash table

--- hash_table/hash_table.cpp
+++ hash_table/hash_table.cpp
@@ -13,7 +13,18 @@ Status HashTable::create(std::string db_path) {
 			f.close();
 			file.open(db_path);
 			initialize_new_db();
+		} else {
+			//get the number of buckets from an already established database
+			file.seekg(0, std::ios::beg);
+
+			std::string header;
+			std::getline(file, header);
+
+			int end = std::find(header.begin(), header.end(), ' ') - header.begin();
+			NUMBER_OF_BUCKETS = std::stoi(header.substr(0, end));
+			file.seekg(0, std::ios::beg);
 		}
+
 	} catch (std::exception e) {
 		return Status::Error(e.what());
 	}
@@ -77,21 +88,11 @@ Status HashTable::put(std::string key, std::string value) {
 		return Status::Error("DB not opened.");
 
 	if (key.size() > 9 || value.size() > 9)
-		return Status::Error("Key/Value Size Limit Exceeded.");
-
-	int hash_value = HashTable::hash(key);
+		return Status::Error("Key/Value Size Must be <= 10.");
 
 	try {
-		file.seekg(0, std::ios::beg);
-
-		std::string header;
-		std::getline(file, header);
-
-		int end = std::find(header.begin(), header.end(), ' ') - header.begin();
-		int number_of_buckets = std::stoi(header.substr(0, end));
-
-		int bucket_alloted = hash_value % number_of_buckets;
-		int position_in_file = OFFSET_FROM_HEADER + bucket_alloted * BUCKET_SIZE;
+		int bucket_allotted = HashTable::hash(key) % NUMBER_OF_BUCKETS;
+		int position_in_file = OFFSET_FROM_HEADER + bucket_allotted * BUCKET_SIZE;
 
 		file.seekg(position_in_file, std::ios::beg);
 
@@ -105,10 +106,57 @@ Status HashTable::put(std::string key, std::string value) {
 		file << "1\n";
 		file.seekg(file.tellg(), std::ios::beg);
 
-		file.write(key.c_str(), key.size());				//write the key
+		file << key;				//write the key
 		file.seekp(10 - key.size(), std::ios::cur);			//move to the next line
 
-		file.write(value.c_str(), value.size());
+		file << value;
+
+	} catch (std::exception e) {
+		return Status::Error(e.what());
+	}
+
+	return Status::OK();
+}
+
+Status HashTable::get(std::string key, std::string* value) {
+	if (!file.is_open())
+		return Status::Error("DB not opened.");
+
+	if (key.size() > 9)
+		return Status::Error("Key/Value Size Must be <= 10.");
+
+	try {
+		int bucket_allotted = HashTable::hash(key) % NUMBER_OF_BUCKETS;
+		int position_in_file = OFFSET_FROM_HEADER + bucket_allotted * BUCKET_SIZE;
+
+		file.seekg(position_in_file, std::ios::beg);
+
+		char bucket_flag;
+		file.get(bucket_flag);
+
+		if (bucket_flag == '0') {
+			*value = "";
+			return Status::OK();					//key not in table
+		}
+
+		file.seekg(1, std::ios::cur);
+
+		std::string key_in_bucket;
+		std::getline(file, key_in_bucket);			//read the key
+
+		int end = std::find(key_in_bucket.begin(), key_in_bucket.end(), ' ') - key_in_bucket.begin();
+		key_in_bucket = key_in_bucket.substr(0, end);
+
+		if (key != key_in_bucket) {
+			*value = "";
+			return Status::OK();
+		}
+
+		std::string value_in_bucket;
+		std::getline(file, value_in_bucket);		//read the value
+
+		end = std::find(value_in_bucket.begin(), value_in_bucket.end(), ' ') - value_in_bucket.begin();
+		*value = value_in_bucket.substr(0, end);
 
 	} catch (std::exception e) {
 		return Status::Error(e.what());
--- tests/hash_table_test.h
+++ tests/hash_table_test.h
@@ -19,33 +19,50 @@ TEST(HashTable, hash_function) {
 	EXPECT_EQ(expected_hash, HashTable::hash(key));
 }
 
-TEST(HashTable, put) {
+TEST(HashTable, put_get) {
 	HashTable h;
 	h.create("test_table.dat");
 
-	Status s = h.put("abc", "123");
-	ASSERT_TRUE(s) << s.to_string();
-	EXPECT_EQ(123, h.file_size());
+	{
+		Status s = h.put("abc", "123");
+		ASSERT_TRUE(s) << s.to_string();
+		EXPECT_EQ(123, h.file_size());
 
-	s = h.put("abc", "124");
-	ASSERT_TRUE(!s);
-	EXPECT_EQ("Error: ERROR - Bucket Full", s.to_string());
+		s = h.put("abc", "124");
+		ASSERT_TRUE(!s);
+		EXPECT_EQ("Error: ERROR - Bucket Full", s.to_string());
 
-	s = h.put("1234567890", "abc");
-	ASSERT_TRUE(!s);
-	EXPECT_EQ("Error: ERROR - Key/Value Size Limit Exceeded.", s.to_string());
+		s = h.put("1234567890", "abc");
+		ASSERT_TRUE(!s);
+		EXPECT_EQ("Error: ERROR - Key/Value Size Must be <= 10.", s.to_string());
 
-	s = h.put("a", "1234567890");
-	ASSERT_TRUE(!s);
-	EXPECT_EQ("Error: ERROR - Key/Value Size Limit Exceeded.", s.to_string());
+		s = h.put("a", "1234567890");
+		ASSERT_TRUE(!s);
+		EXPECT_EQ("Error: ERROR - Key/Value Size Must be <= 10.", s.to_string());
 
-	s = h.put("6", "123");
-	ASSERT_TRUE(s) << s.to_string();
-	EXPECT_EQ(123, h.file_size());
+		s = h.put("6", "abcd");
+		ASSERT_TRUE(s) << s.to_string();
+		EXPECT_EQ(123, h.file_size());
 
-	s = h.put("123", "123");
-	ASSERT_TRUE(s) << s.to_string();
-	EXPECT_EQ(123, h.file_size());
+		s = h.put("123", "123");
+		ASSERT_TRUE(s) << s.to_string();
+		EXPECT_EQ(123, h.file_size());
+	}
+
+	{
+		std::string value;
+		Status s = h.get("abc", &value);
+		ASSERT_TRUE(s) << s.to_string();
+		EXPECT_EQ("123", value);
+
+		s = h.get("6", &value);
+		ASSERT_TRUE(s) << s.to_string();
+		EXPECT_EQ("abcd", value);
+
+		s = h.get("69", &value);
+		ASSERT_TRUE(s) << s.to_string();
+		EXPECT_EQ("", value);
+	}
 
 	h.remove_table();
 }
